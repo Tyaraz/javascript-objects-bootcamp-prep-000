e6f007ab9ce80e23563762886778ef2954fd954d
Tya Raz <tyaraz.rocketmail@gmail.com>
2018-06-20 07:26:54 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,11 +4,13 @@ var playlist = {
 }
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist[artistName] = songTitle;
   return playlist;
 }
+
+updatePlaylist(playlist, 'Adele', 'Hello')
 
 function removeFromPlaylist(playlist, artistName) {
   delete playlist[artistName];
   return playlist;
 }
+
